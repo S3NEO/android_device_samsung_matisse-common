22a36b731dc0d00b7c7be184c53f4353d83dfec5
Alistair Strachan <astrachan@google.com>
2019-01-16 14:10:47 -0800

    Revert "matisse-common: Use first_stage_mount feature"
    This reverts commit 3bb99e88ebd0291a59633cb38dca161410ed6523.

--- matisse.mk
+++ matisse.mk
@@ -84,7 +84,6 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
-	$(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
     init.target.rc
 
 # Recents
