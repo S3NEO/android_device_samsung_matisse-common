8b09473b423eae3cfbe2b8055920463998e7fe21
RomanDesigner <romashkarus08@gmail.com>
2020-01-17 16:25:31 +0300

    Init bringup for Q

--- init/init_matisse.cpp
+++ init/init_matisse.cpp
@@ -28,61 +28,103 @@
  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <stdlib.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
+#include <android-base/file.h>
 #include <android-base/logging.h>
 #include <android-base/properties.h>
+#include <android-base/strings.h>
+
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
 
-#include "init_msm8226.h"
-
 using android::base::GetProperty;
+using android::base::ReadFileToString;
+using android::base::Trim;
 using android::init::property_set;
 
-void gsm_properties()
+// copied from build/tools/releasetools/ota_from_target_files.py
+// but with "." at the end and empty entry
+std::vector<std::string> ro_product_props_default_source_order = {
+    "",
+    "product.",
+    "product_services.",
+    "odm.",
+    "vendor.",
+    "system.",
+};
+
+void property_override(char const prop[], char const value[], bool add = true)
+{
+    auto pi = (prop_info *) __system_property_find(prop);
+
+    if (pi != nullptr) {
+        __system_property_update(pi, value, strlen(value));
+    } else if (add) {
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
+}
+
+void set_wifionly()
 {
     property_set("ro.carrier", "wifi-only");
     property_set("ro.radio.noril", "1");
 }
 
-void init_target_properties()
+void vendor_load_properties()
 {
-	std::string platform = GetProperty("ro.board.platform", "");
-	if (platform != ANDROID_TARGET)
-		return;
 	
+    const auto set_ro_product_prop = [](const std::string &source,
+            const std::string &prop, const std::string &value) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
 	std::string bootloader = GetProperty("ro.bootloader", "");
 	if (bootloader.find("T530NU") == 0) {
 		/* matissewifiue */
-		property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/matissewifiue/matissewifi:5.0.2/LRX22G/T530NUU1BOJ4:user/release-keys");
+        for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_product_prop(source, "fingerprint", "samsung/matissewifiue/matissewifi:5.0.2/LRX22G/T530NUU1BOJ4:user/release-keys");
+            set_ro_product_prop(source, "model", "SM-T530NU");
+            set_ro_product_prop(source, "device", "matissewifi");
+        }
 		property_override("ro.build.description", "matissewifiue-user 5.0.2 LRX22G T530NUU1BOJ4 release-keys");
-		property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-T530NU");
-		property_override_dual("ro.product.device", "ro.vendor.product.device", "matissewifi");
-		gsm_properties();
+		set_wifionly();
 	} else if (bootloader.find("T530XX") == 0) {
 		/* matissewifixx */
-		property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/matissewifixx/matissewifi:5.0.2/LRX22G/T530XXU1BOJ4:user/release-keys");
+        for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_product_prop(source, "fingerprint", "samsung/matissewifixx/matissewifi:5.0.2/LRX22G/T530XXU1BOJ4:user/release-keys");
+            set_ro_product_prop(source, "model", "SM-T530");
+            set_ro_product_prop(source, "device", "matissewifi");
+        }
 		property_override("ro.build.description", "matissewifixx-user 5.0.2 LRX22G T530XXU1BOJ4 release-keys");
-		property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-T530");
-		property_override_dual("ro.product.device", "ro.vendor.product.device", "matissewifi");
-		gsm_properties();
+		set_wifionly();
 	} else if (bootloader.find("T531XX") == 0) {
 		/* matisse3gxx */
-		property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/matisse3gxx/matisse3g:5.0.2/LRX22G/T531XXU1BOE6:user/release-keys");
+        for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_product_prop(source, "fingerprint", "samsung/matisse3gxx/matisse3g:5.0.2/LRX22G/T531XXU1BOE6:user/release-keys");
+            set_ro_product_prop(source, "model", "SM-T531");
+            set_ro_product_prop(source, "device", "matisse3g");
+        }
 		property_override("ro.build.description", "matisse3gxx-user 5.0.2 LRX22G T531XXU1BOE6 release-keys");
-		property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-T531");
-		property_override_dual("ro.product.device", "ro.vendor.product.device", "matisse3g");
 	} else if (bootloader.find("T535XX") == 0) {
 		/* matisseltexx */
-		property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/matisseltexx/matisselte:5.0.2/LRX22G/T535XXU1BOL1:user/release-keys");
+        for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_product_prop(source, "fingerprint", "samsung/matisseltexx/matisselte:5.0.2/LRX22G/T535XXU1BOL1:user/release-keys");
+            set_ro_product_prop(source, "model", "SM-T535");
+            set_ro_product_prop(source, "device", "matisselte");
+        }
 		property_override("ro.build.description", "matisseltexx-user 5.0.2 LRX22G T535XXU1BOL1 release-keys");
-		property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-T535");
-		property_override_dual("ro.product.device", "ro.vendor.product.device", "matisselte");
 	} else {
-		gsm_properties();
+		set_wifionly();
 	}
 	
 	std::string device = GetProperty("ro.product.device", "");
