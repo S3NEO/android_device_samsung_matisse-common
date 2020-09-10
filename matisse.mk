17ba3114c7fe4d0336cd161b3612c541aae5caee
matteo0026 <marmotta200026@gmail.com>
2020-09-10 22:46:44 +0200

    matisse-common: converto audio_effects.conf to audio_effects.xml * Converted .conf format to .xml using: https://github.com/luk1337/aeffects-conf2xml

--- matisse.mk
+++ matisse.mk
@@ -26,7 +26,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
