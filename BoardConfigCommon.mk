6f82554da3af99f0dfbdcc1237c96231eab3ac93
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-10-02 17:03:28 +0200

    DNM: hacks

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,7 +46,7 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 zcache.enabled=1 zcache.compressor=lz4 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x37 zcache.enabled=1 zcache.compressor=lz4 androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
@@ -70,7 +70,7 @@ TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
+# BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
 BOARD_CACHEIMAGE_PARTITION_SIZE := 314572800
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2362232012
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12884901888
